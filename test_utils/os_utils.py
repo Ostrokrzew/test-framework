4cae0a237c395088d2af033541a32fe5a7453b9e
Slawomir Jankowski <slawomir.jankowski@intel.com>
2021-01-08 13:42:19 +0100

    Add missing import
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

--- test_utils/os_utils.py
+++ test_utils/os_utils.py
@@ -13,8 +13,11 @@ from packaging import version
 from core.test_run import TestRun
 from storage_devices.device import Device
 from test_tools.dd import Dd
+from test_tools.fs_utils import (
+    check_if_directory_exists, FileType, find, create_directory, ls, parse_ls_output, read_file,
+    check_if_file_exists
+)
 from test_tools.disk_utils import get_sysfs_path
-from test_tools.fs_utils import check_if_directory_exists, create_directory, check_if_file_exists
 from test_utils.filesystem.file import File
 from test_utils.output import CmdException
 from test_utils.size import Size, Unit
